3973bfd8b692f3ea5a1bed1b6f6047a150d27e9b
Ahmed <ahmed91alhaj@gmail.com>
2023-06-21 05:57:18 -0400

    Add RESULTS & ATTEMPTS tables

---
+++ db/schema/05_results.sql
@@ -0,0 +1,9 @@
+-- Drop and recreate Widgets table (Example)
+
+DROP TABLE IF EXISTS results CASCADE;
+CREATE TABLE results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  quiz_id INTEGER REFERENCES quizes(id) ON DELETE CASCADE,
+  score INTEGER NOT NULL
+);
