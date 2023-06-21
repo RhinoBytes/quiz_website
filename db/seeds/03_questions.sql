6047ef0c4a26790fbf89197fa8e526a8fd90dba7
Doug Ross <dross.canada@gmail.com>
2023-06-21 17:41:53 -0400

    update seeds to populate database with user data

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -14,49 +14,49 @@ VALUES ('What is the result of 25 / 5?', 1);
 
 -- quiz 2
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the capital of France?', 6);
+VALUES ('What is the capital of France?', 2);
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the capital of Japan?', 7);
+VALUES ('What is the capital of Japan?', 2);
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the capital of Australia?', 8);
+VALUES ('What is the capital of Australia?', 2);
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the capital of Brazil?', 9);
+VALUES ('What is the capital of Brazil?', 2);
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the capital of Egypt?', 10);
+VALUES ('What is the capital of Egypt?', 2);
 
 -- quiz 3
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the largest land animal?', 1);
+VALUES ('What is the largest land animal?', 3);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which animal has the longest neck?', 1);
+VALUES ('Which animal has the longest neck?', 3);
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the tallest animal in the world?', 1);
+VALUES ('What is the tallest animal in the world?', 3);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which animal is known for having a trunk?', 1);
+VALUES ('Which animal is known for having a trunk?', 3);
 INSERT INTO questions (text, quiz_id)
-VALUES ('What is the heaviest animal on Earth?', 1);
+VALUES ('What is the heaviest animal on Earth?', 3);
 
 -- quiz 4
 
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which province is known for its maple syrup production?', 1);
+VALUES ('Which province is known for its maple syrup production?', 4);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which province is the largest by land area?', 1);
+VALUES ('Which province is the largest by land area?', 4);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which province is known for its Rocky Mountains?', 1);
+VALUES ('Which province is known for its Rocky Mountains?', 4);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which province is the most populous in Canada?', 1);
+VALUES ('Which province is the most populous in Canada?', 4);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which province is the only officially bilingual province?', 1);
+VALUES ('Which province is the only officially bilingual province?', 4);
 
 -- quiz 5
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which bird is known for its ability to mimic human speech?', 1);
+VALUES ('Which bird is known for its ability to mimic human speech?', 5);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which bird has the largest wingspan of any living bird?', 1);
+VALUES ('Which bird has the largest wingspan of any living bird?', 5);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which bird is the national bird of the United States?', 1);
+VALUES ('Which bird is the national bird of the United States?', 5);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which bird is known for its dance-like mating display?', 1);
+VALUES ('Which bird is known for its dance-like mating display?', 5);
 INSERT INTO questions (text, quiz_id)
-VALUES ('Which bird is the fastest animal on two legs?', 1);
+VALUES ('Which bird is the fastest animal on two legs?', 5);
