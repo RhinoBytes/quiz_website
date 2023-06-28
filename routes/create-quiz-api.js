f1187c39bec0af7fad51a8a8715eb74a19676fbd
Doug Ross <dross.canada@gmail.com>
2023-06-27 22:06:13 -0400

    fix insert choices errror

--- routes/create-quiz-api.js
+++ routes/create-quiz-api.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router  = express.Router();
-const db = require('../db/connection');
+const router = express.Router();
+const db = require('../db/queries/users');
 
 function processFormData(req) {
   const quizTitle = req.body['quiz-title'];
@@ -8,18 +8,19 @@ function processFormData(req) {
   const username = 'Doug';
 
   const questions = [];
+  //
   for (let i = 1; i <= 5; i++) {
     const question = req.body[`question-${i}`];
     const answers = [];
-    let correctAnswer = null;
+    let correctAnswerIndex = null;
     for (let j = 1; j <= 4; j++) {
       const answer = req.body[`answer-${i}-${j}`];
       answers.push(answer);
       if (req.body[`correct-answer-${i}`] === `${j}`) {
-        correctAnswer = j;
+        correctAnswerIndex = j - 1;
       }
     }
-    questions.push({ question, answers, correctAnswer });
+    questions.push({ question, answers, correctAnswer: correctAnswerIndex });
   }
 
   const creationDate = new Date();
@@ -38,7 +39,7 @@ function processFormData(req) {
 }
 
 router.post('/', async (req, res) => {
-  console.log("error post")
+  console.log("error post");
   try {
     const {
       quizTitle,
@@ -62,10 +63,16 @@ router.post('/', async (req, res) => {
     console.log('Quiz inserted. QuizID:', quizId);
 
     for (const question of questions) {
-      await db.insertQuestion(question.question, quizId);
+      const questionId = await db.insertQuestion(question.question, quizId);
       console.log('Question inserted:', question.question);
+
+      for (let i = 0; i < question.answers.length; i++) {
+        const isCorrectAnswer = i === question.correctAnswer;
+        await db.insertChoice(question.answers[i], isCorrectAnswer, questionId);
+        console.log('Choice inserted:', question.answers[i], 'Is correct:', isCorrectAnswer);
+      }
     }
-    res.sendStatus(200);
+    res.status(200).json({quizId});
 
   } catch (error) {
     console.error('Error creating quiz:', error);
